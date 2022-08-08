1481d89387fe9274e59c219f2944657df51e5290
florpalma1 <contr.lucascordoba98@gmail.com>
2022-08-08 17:57:23 -0300

    ejercicio 3 completo

---
+++ js/e-3.js
@@ -0,0 +1,40 @@
+// 3-Escribe una clase que permita crear distintos objetos “rectángulos”, con las propiedades de alto y ancho, mas los métodos necesarios para modificar y mostrar sus propiedades, calcular el perímetro y el área
+class Rectangulo {
+    constructor(alto, ancho) {
+        this.alto = alto;
+        this.ancho = ancho;
+    }
+    modificarAlto(altura) {
+        this.alto = altura;
+    }
+    modificarAncho(ancho) {
+        this.ancho = ancho;
+    }
+    calculoArea() {
+        this.area = (this.alto * this.ancho);
+    }
+    calculoPerimetro() {
+        this.perimetro = (this.alto * 2 + this.ancho * 2);
+    }
+    mostrarPropiedades() {
+        document.write(`
+      RECTANGULO
+      <ul>
+      <li>Alto: ${this.alto}</li>
+      <li>Ancho: ${this.ancho}</li>
+      <li>Area: ${this.area}</li>
+      <li>Perimetro: ${this.perimetro}</li>
+      </ul>
+      `);
+    }
+
+}
+let TrianguloA = new Rectangulo(12, 6);
+TrianguloA.calculoArea();
+TrianguloA.calculoPerimetro();
+TrianguloA.mostrarPropiedades();
+TrianguloA.modificarAlto(4);
+TrianguloA.modificarAncho(2);
+TrianguloA.calculoArea();
+TrianguloA.calculoPerimetro();
+TrianguloA.mostrarPropiedades();
